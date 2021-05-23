4acfe042ca3de4020106f57e7a57bd00c3a47533
greksadarko <greksadarko@gmail.com>
2021-05-23 19:58:54 +0200

    Initial commit

---
+++ src/main/java/com/example/tseo/TseoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tseo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TseoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TseoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tseo/TseoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TseoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
