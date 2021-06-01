7c7583c00769199c263f9f55d648ec5bfeb16a42
greksadarko <greksadarko@gmail.com>
2021-06-01 17:32:17 +0200

    Zapocet rad sa kontrolerima

---
+++ tseo/src/main/java/com/example/tseo/service/StudentService.java
@@ -0,0 +1,4 @@
+package com.example.tseo.service;
+
+public class StudentService {
+}
---
+++ tseo/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO ispitni_rokovi (naziv, datum_pocetka, datum_zavrsetka) VALUES ('Januarski', '2021-01-15', '2021-01-31');
+INSERT INTO ispitni_rokovi (naziv, datum_pocetka, datum_zavrsetka) VALUES ('Februarski', '2021-02-01', '2019-02-20');
+INSERT INTO ispitni_rokovi (naziv, datum_pocetka, datum_zavrsetka) VALUES ('Junski', '2021-06-14', '2019-06-30');
+INSERT INTO ispitni_rokovi (naziv, datum_pocetka, datum_zavrsetka) VALUES ('Julski', '2021-07-01', '2021-07-20');
+INSERT INTO ispitni_rokovi (naziv, datum_pocetka, datum_zavrsetka) VALUES ('Septembarski', '2021-09-01', '2021-09-20');
