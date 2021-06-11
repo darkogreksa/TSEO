eea71200956d9d0df0dfbd918bfa773051ad7766
greksadarko <greksadarko@gmail.com>
2021-06-11 17:29:34 +0200

    Povezivanje sa bazom sitne izmene

--- tseo/src/main/resources/import.sql
+++ tseo/src/main/resources/import.sql
@@ -23,54 +23,17 @@ INSERT INTO predmet (naziv, espb, sifra_predmeta, godina, smer) VALUES ('Tehnolo
 INSERT INTO izvedbe (pocetak, kraj, obrisan, predmet_id) VALUES ('2021-01-20', '2021-06-15', false, 1);
 INSERT INTO izvedbe (pocetak, kraj, obrisan, predmet_id) VALUES ('2021-01-20', '2021-06-15', false, 2);
 INSERT INTO izvedbe (pocetak, kraj, obrisan, predmet_id) VALUES ('2021-01-20', '2021-06-15', false, 3);
-INSERT INTO izvedbe (pocetak, kraj, obrisan, predmet_id) VALUES ('2021-01-20', '2021-06-15', false, 4);
-INSERT INTO izvedbe (pocetak, kraj, obrisan, predmet_id) VALUES ('2021-01-20', '2021-06-15', false, 5);
-INSERT INTO izvedbe (pocetak, kraj, obrisan, predmet_id) VALUES ('2021-01-20', '2021-06-15', false, 6);
 
 INSERT INTO predavanja (obrisan, izvedba_id, nastavnik_id) VALUES (false, 1, 1);
 INSERT INTO predavanja (obrisan, izvedba_id, nastavnik_id) VALUES (false, 3, 1);
 INSERT INTO predavanja (obrisan, izvedba_id, nastavnik_id) VALUES (false, 2, 2);
 
-INSERT INTO ispitni_rokovi (naziv, datum_pocetka, datum_zavrsetka) VALUES ('januarski', '2021-06-20', '2021-07-01');
-INSERT INTO ispitni_rokovi (naziv, datum_pocetka, datum_zavrsetka) VALUES ('februarski', '2021-08-20', '2021-09-01');
-INSERT INTO ispitni_rokovi (naziv, datum_pocetka, datum_zavrsetka) VALUES ('junski', '2021-08-20', '2021-09-01');
-INSERT INTO ispitni_rokovi (naziv, datum_pocetka, datum_zavrsetka) VALUES ('julski', '2021-08-20', '2021-09-01');
-INSERT INTO ispitni_rokovi (naziv, datum_pocetka, datum_zavrsetka) VALUES ('novembar 8', '2021-08-20', '2021-09-01');
+INSERT INTO ispit (datum, ucionica, vrsta, predmet_id, rok_id) VALUES ('2021-06-21', 'JUG112', 'Ispit', 1, 3);
+INSERT INTO ispit (datum, ucionica, vrsta, predmet_id, rok_id) VALUES ('2021-06-24', 'JUG103', 'Kolokvijum', 2, 3);
 
-INSERT INTO ispit (datum, ucionica, vrsta, predmet_id, rok_id) VALUES ('2021-06-21', 'JUG112', 'Ispit', 1, 1);
-INSERT INTO ispit (datum, ucionica, vrsta, predmet_id, rok_id) VALUES ('2021-06-24', 'JUG103', 'Kolokvijum', 3, 1);
-INSERT INTO ispit (datum, ucionica, vrsta, predmet_id, rok_id) VALUES ('2021-06-22', 'A2', 'Ispit', 2, 1);
-INSERT INTO ispit (datum, ucionica, vrsta, predmet_id, rok_id) VALUES ('2021-06-22', 'A2', 'Ispit', 5, 1);
-INSERT INTO ispit (datum, ucionica, vrsta, predmet_id, rok_id) VALUES ('2021-06-22', 'A2', 'Ispit', 6, 1);
-INSERT INTO ispit (datum, ucionica, vrsta, predmet_id, rok_id) VALUES ('2021-06-22', 'A2', 'Kolokvijum', 4, 1);
-INSERT INTO ispit (datum, ucionica, vrsta, predmet_id, rok_id) VALUES ('2021-06-22', 'A2', 'Kolokvijum', 5, 1);
-INSERT INTO ispit (datum, ucionica, vrsta, predmet_id, rok_id) VALUES ('2021-06-22', 'A2', 'Ispit', 5, 1);
-
-
-INSERT INTO izlasci (bodovi, ocena, polozio, ispit_id, student_id ) VALUES (80, null, null, 1, 1);
+INSERT INTO izlasci (bodovi, ocena, polozio, ispit_id, student_id ) VALUES (81, 8, true, 1, 1);
 INSERT INTO izlasci (bodovi, ocena, polozio, ispit_id, student_id ) VALUES (20, 5, false, 2, 1);
-INSERT INTO izlasci (bodovi, ocena, polozio, ispit_id, student_id ) VALUES (50, 6, true, 3, 2);
-INSERT INTO izlasci (bodovi, ocena, polozio, ispit_id, student_id ) VALUES (50, 6, false, 2, 3);
-INSERT INTO izlasci (bodovi, ocena, polozio, ispit_id, student_id ) VALUES (50, 6, false, 3, 3);
-INSERT INTO izlasci (bodovi, ocena, polozio, ispit_id, student_id ) VALUES (61, 7, true, 3, 1);
-INSERT INTO izlasci (bodovi, ocena, polozio, ispit_id, student_id ) VALUES (null, null, false, 5, 1);
-INSERT INTO izlasci (bodovi, ocena, polozio, ispit_id, student_id ) VALUES (null, null, false, 2, 1);
-INSERT INTO izlasci (bodovi, ocena, polozio, ispit_id, student_id ) VALUES (null, null, false, 4, 1);
-INSERT INTO izlasci (bodovi, ocena, polozio, ispit_id, student_id ) VALUES (null, null, false, 7, 1);
-INSERT INTO izlasci (bodovi, ocena, polozio, ispit_id, student_id ) VALUES (null, null, false, 5, 1);
-INSERT INTO izlasci (bodovi, ocena, polozio, ispit_id, student_id ) VALUES (null, null, false, 5, 1);
-INSERT INTO izlasci (bodovi, ocena, polozio, ispit_id, student_id ) VALUES (null, null, false, 6, 1);
-INSERT INTO izlasci (bodovi, ocena, polozio, ispit_id, student_id ) VALUES (null, null, false, 2, 2);
-
-INSERT INTO user_authority (user_id, authority_id) VALUES (1, 1);
-INSERT INTO user_authority (user_id, authority_id) VALUES (3, 2);
-INSERT INTO user_authority (user_id, authority_id) VALUES (2, 3);
-
 
 INSERT INTO pohadjanja(izvedba_id, student_id, obrisan) VALUES (1,1,false);
 INSERT INTO pohadjanja(izvedba_id, student_id, obrisan) VALUES (2,1,false);
 INSERT INTO pohadjanja(izvedba_id, student_id, obrisan) VALUES (2,2,false);
-INSERT INTO pohadjanja(izvedba_id, student_id, obrisan) VALUES (3,3,false);
-INSERT INTO pohadjanja(izvedba_id, student_id, obrisan) VALUES (3,1,false);
-INSERT INTO pohadjanja(izvedba_id, student_id, obrisan) VALUES (4,1,false);
-INSERT INTO pohadjanja(izvedba_id, student_id, obrisan) VALUES (5,1,false);
